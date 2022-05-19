f973ac82485f6288c9e5783ae11d9d64feba938c
Abhay Joshi <abhay29082003@gmail.com>
2022-05-19 17:12:43 +0530

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
 #include<iostream>
+#include<SDL.h>
+#include<imgui.h>
+#include<imgui_impl_sdl.h>
+#include<imgui_impl_sdlrenderer.h>
 
-int main()
+int main(int, char**)
 {
-    std::cout << "Hello World!" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
-    std::cin.get();
+    SDL_Window* window = SDL_CreateWindow("Imgui-Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == NULL)
+    {
+        std::cout << "ERROR: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //setup imgui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+
+
+    bool running = true;
+
+    while(running)
+    {
+        SDL_Event event;
+
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        std::cout << "1" << std::endl;
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
